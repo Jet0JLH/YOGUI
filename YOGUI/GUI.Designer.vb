ba01328100df5ea4d144dda987b98d033fce4784
Jet0JLH <Jet0JLH@gmx.de>
2020-08-03 14:34:23 +0200

    add getTabTemplate

--- YOGUI/GUI.Designer.vb
+++ YOGUI/GUI.Designer.vb
@@ -22,17 +22,29 @@ Partial Class GUI
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(835, 650)
+        Me.TabControl1.TabIndex = 0
         '
         'GUI
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(644, 548)
+        Me.ClientSize = New System.Drawing.Size(859, 674)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "GUI"
         Me.Text = "GUI"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents TabControl1 As TabControl
 End Class
--- YOGUI/GUI.vb
+++ YOGUI/GUI.vb
@@ -2,4 +2,17 @@
     Private Sub GUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub GUI_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+
+    End Sub
+
+    Public Function getTabTemplate(ByVal name As String) As TabPage
+        Dim tabTemplate As New TabPage(name)
+        Dim layoutPanel As New FlowLayoutPanel()
+        layoutPanel.Dock = DockStyle.Fill
+        layoutPanel.FlowDirection = FlowDirection.TopDown
+        tabTemplate.Controls.Add(layoutPanel)
+        Return tabTemplate
+    End Function
 End Class
